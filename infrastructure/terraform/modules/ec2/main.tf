aa729e25c70ca0d9631f77e3582a801a200455d3
Kawan Silva <kwnsilva@hotmail.com>
2026-04-06 08:40:02 -0300

    fix: setup job working-directory override

--- infrastructure/terraform/modules/ec2/main.tf
+++ infrastructure/terraform/modules/ec2/main.tf
@@ -4,7 +4,17 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
